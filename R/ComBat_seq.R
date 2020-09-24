0a5550c6ff39ac61aa12306774b7d04eedf69ee3
zhangyuqing <zhangyuqing.pkusms@gmail.com>
2020-09-23 17:29:21 -0700

    fix issue for data.frame input format

--- R/ComBat_seq.R
+++ R/ComBat_seq.R
@@ -36,6 +36,8 @@
 ComBat_seq <- function(counts, batch, group=NULL, covar_mod=NULL, full_mod=TRUE, 
                        shrink=FALSE, shrink.disp=FALSE, gene.subset.n=NULL){  
   ########  Preparation  ########  
+  counts <- as.matrix(counts)
+  
   ## Does not support 1 sample per batch yet
   batch <- as.factor(batch)
   if(any(table(batch)<=1)){
